4ada364a0f7818f8f6e11c081790ec33263dc7e9
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-02-18 23:03:35 +0100

    feat : add Layout with navigation and logo inside

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import MainNavigation from "./main-navigation";
+
+function Layout(props) {
+  return (
+    <div>
+      <MainNavigation />
+      <main>{props.children}</main>
+    </div>
+  );
+}
+
+export default Layout;
