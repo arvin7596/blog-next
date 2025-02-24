1a9d340dc349bad6c09661a251326f1b50750f25
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-02-24 23:36:05 +0100

    refactor : add head to all pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
+import Head from "next/head";
 
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.featuredPosts} />
     </>
